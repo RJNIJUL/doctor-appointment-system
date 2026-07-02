9782f2edef77a62a308655898ecfc7d4cdd1295c
Nijul jamwal <nijuljamwal555@gmail.com>
2026-07-02 17:51:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ class Config:
     SECRET_KEY = 'your_secret_key_here'
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'abc@123'  # put your actual MySQL password here
+    MYSQL_PASSWORD = ''  # put your actual MySQL password here
     MYSQL_DB = 'doctor_appointment'
